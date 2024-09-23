9427766d09451f66718ef22f709f94dc884e1982
Jeff Lamb <jeff.lamb@normalvr.com>
2024-09-23 15:13:46 -0700

    testing text on mobile

--- scripts/main.js
+++ scripts/main.js
@@ -77,7 +77,8 @@ function parseCSV(data) {
 
         result.push({
             word: word.trim(),         // Remove any surrounding whitespace from the word
-            hint: hint.trim(),         // Remove any surrounding whitespace from the hint
+            //hint: hint.trim(),         // Remove any surrounding whitespace from the hint
+            hint: "This is a very long example hint that needs to scale down",
             scrambled: scrambled.trim()  // Remove any surrounding whitespace from the scrambled word
         });
     }
